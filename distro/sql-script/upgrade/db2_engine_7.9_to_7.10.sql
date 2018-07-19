3a4cc7cff3c696d4a2361d2b1ddb565166ecff86
Nikola Koevski <koevskin@gmail.com>
2018-07-19 14:39:37 +0200

    Add root id attribute and index to upgrade sql scripts

--- distro/sql-script/upgrade/db2_engine_7.9_to_7.10.sql
+++ distro/sql-script/upgrade/db2_engine_7.9_to_7.10.sql
@@ -4,3 +4,8 @@ drop index ACT_IDX_JOB_HANDLER;
 -- https://app.camunda.com/jira/browse/CAM-9084
 ALTER TABLE ACT_RE_PROCDEF
   ADD STARTABLE_ smallint DEFAULT 1 not null check(STARTABLE_ in (1,0));
+
+-- https://app.camunda.com/jira/browse/CAM-9199
+ALTER TABLE ACT_HI_PROCINST
+  ADD ROOT_PROCESS_INSTANCE_ID_ varchar(64);
+create index ACT_IDX_HI_PRO_INST_ROOT_PI on ACT_HI_PROCINST(ROOT_PROCESS_INSTANCE_ID_);
--- distro/sql-script/upgrade/mssql_engine_7.9_to_7.10.sql
+++ distro/sql-script/upgrade/mssql_engine_7.9_to_7.10.sql
@@ -1,3 +1,8 @@
 -- https://app.camunda.com/jira/browse/CAM-9084
 ALTER TABLE ACT_RE_PROCDEF
   ADD STARTABLE_ bit NOT NULL DEFAULT 1;
+
+-- https://app.camunda.com/jira/browse/CAM-9199
+ALTER TABLE ACT_HI_PROCINST
+  ADD ROOT_PROCESS_INSTANCE_ID_ nvarchar(64);
+create index ACT_IDX_HI_PRO_INST_ROOT_PI on ACT_HI_PROCINST(ROOT_PROCESS_INSTANCE_ID_);
